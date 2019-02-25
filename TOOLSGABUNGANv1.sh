1441e13090e2b3808eaf20a58d14b5d5bb6a3a32
TU4NG4L4U <46773642+TU4NG4L4U@users.noreply.github.com>
2019-02-25 14:59:43 +0700

    Add files via upload

---
+++ TOOLSGABUNGANv1.sh
@@ -0,0 +1,217 @@
+echo "______________________________________________________" | lolcat
+echo
+echo "🔰Athuar : TU4N G4L4U " | lolcat
+echo
+echo "🔰TOOLS GABUNGNv1 " | lolcat
+echo
+echo "🔰WA : 081514074360" | lolcat
+echo
+echo "🔰TEAM : ELANG CYBER TEAM " | lolcat
+echo
+echo "🔰FB : Hai" | lolcat
+echo
+echo "🔰Github : https://github.com/TU4NG4L4U " | lolcat
+echo
+echo "______________________________________________________" | lolcat
+echo
+echo "__1.Report FB " | lolcat
+echo "__2.Auto Like FB " | lolcat
+echo "__3.Spam SMS " |lolcat
+echo "__4.Gcospam" | lolcat
+echo "__5.MBF " | lolcat
+echo "__6.FB Crack" | lolcat
+echo "__7.OSIF " | lolcat
+echo "__8.Merubah Tampilan Termux" | lolcat
+echo "__9.Nampilin Kereta " | lolcat
+echo "__10.MoonBugy (GAME) " | lolcat
+echo "__11.Hack Wifi (ROOT) " |lolcat
+echo "__12.Installer TU4N G4L4U " | lolcat
+echo "__13.Tools SK4T3RS " | lolcat
+echo "__14.WebDav" | lolcat
+echo
+read -p "Root@TU4NG4L4U~# " pilih
+if [ $pilih = 1 ]
+then
+clear
+figlet -f sleant TU4N G4L4U | lolcat
+echo
+echo
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+echo "Sabar.....!!!"
+echo
+sleep 1
+pkg install curl
+apt instal git php curl -y
+apt install git
+git clone http://github.com/AMVengeance/FB-React.git
+cd FB-React
+chmod +x start
+./start
+fi
+
+if [ $pilih  = 3 ]
+then
+clear
+echo
+echo
+sleep 1
+kg install php && pkg install git
+git clone https://github.com/Thaniiaa/b
+cd b
+php dg.php
+fi
+
+if [ $pilih = 4 ]
+then
+clear
+echo
+echo
+sleep 1
+apt install git
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh instll.sh
+sh gco.sh
+fi
+
+if [ $pilih = 5 ]
+then
+clear
+echo
+echo
+sleep 1
+pkg install git
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+
+if [ $pilih = 6 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loading...!!"
+echo ""
+sleep 1
+apt install git
+pkg install python2
+pip2 install mechanize
+git clone https://github.com/rickyricko302/fb-crackv1
+cd fb-crackv1
+python2 fb-crack.py
+fi
+
+
+if [ $pilih = 7 ]
+then
+clear
+echo
+echo
+sleep 1
+apt update && apt upgrade
+apt install python2
+pip2 install requests mechanize
+apt install git
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+
+if [ $pilih = 8 ]
+then
+echo
+echo
+sleep 1
+pkg install git
+git clone https://github.com/TU4NG4L4U/Termux-color
+cd Termux-color
+sh Termux-color.sh
+fi
+
+
+if [ $pilih = 9 ]
+then
+echo
+echo
+sleep 1
+pkg install sl
+sl
+fi
+
+if [ $pilih = 10 ]
+then
+clear
+echo
+echo
+sleep 1
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $pilih = 11 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo "Loading....!!!"
+echo ""
+sleep 1
+pkg install git
+git clone https://github.com/derv82/wifite
+cd wifite
+chmod 777 wifite.py
+python2 wifite.py
+./wifite.py
+fi
+
+if [ $pilih = 12 ]
+then
+clear
+sleep 1
+pkg install git
+git clone https://github.com/TU4NG4L4U/InstallerV6
+cd InstallerV6
+sh InstallerV6.sh
+fi
+
+if [ $pilih = 13 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding....!!!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/Sk4ters38/SK4T3R5v1
+cd SK4T3R5v1
+sh SK4T3RS.sh
+fi
+
+if [ $pilih = 14 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo ""
+echo "Loding...............!!!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/TUANB4DUT/WEBDAV
+cd WEBDAV
+sh webdav.sh
+fi
+
+if
